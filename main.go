1a5ccb5db91ad8dbbc4b56279ba87396ee1af799
Peter Braun <pbraun@redhat.com>
2025-02-23 12:02:17 +0100

    fix texture calculations

--- main.go
+++ main.go
@@ -218,7 +218,7 @@ func (g *Game) Update() error {
 func texturedTriangle(x1, y1 int, u1, v1 float64,
 	x2, y2 int, u2, v2 float64,
 	x3, y3 int, u3, v3 float64,
-	tex *ebiten.Image) {
+	tex *ebiten.Image, screen *ebiten.Image) {
 
 	if y2 < y1 {
 		y1, y2 = y2, y1
@@ -229,7 +229,7 @@ func texturedTriangle(x1, y1 int, u1, v1 float64,
 
 	if y3 < y1 {
 		y1, y3 = y3, y1
-		x1, x2 = x3, x1
+		x1, x3 = x3, x1
 		u1, u3 = u3, u1
 		v1, v3 = v3, v1
 	}
@@ -251,8 +251,6 @@ func texturedTriangle(x1, y1 int, u1, v1 float64,
 	dv2 := v3 - v1
 	du2 := u3 - u1
 
-	var tex_u, tex_v, tex_w float64
-
 	var dax_step = float64(0)
 	var dbx_step = float64(0)
 
@@ -260,34 +258,35 @@ func texturedTriangle(x1, y1 int, u1, v1 float64,
 	var dv1_step = float64(0)
 	var du2_step = float64(0)
 	var dv2_step = float64(0)
-	var dw1_step = float64(0)
-	var dw2_step = float64(0)
 
-	if dy1 != 0 {
+	if dy1 > 0 {
 		dax_step = float64(dx1) / math.Abs(float64(dy1))
 	}
-	if dy2 != 0 {
+
+	if dy2 > 0 {
 		dbx_step = float64(dx2) / math.Abs(float64(dy2))
 	}
 
-	if dy1 != 0 {
+	if dy1 > 0 {
 		du1_step = float64(du1) / math.Abs(float64(dy1))
 	}
-	if dy1 != 0 {
+
+	if dy1 > 0 {
 		dv1_step = float64(dv1) / math.Abs(float64(dy1))
 	}
 
-	if dy2 != 0 {
+	if dy2 > 0 {
 		du2_step = float64(du2) / math.Abs(float64(dy2))
 	}
-	if dy2 != 0 {
+
+	if dy2 > 0 {
 		dv2_step = float64(dv2) / math.Abs(float64(dy2))
 	}
 
-	if dy1 != 0 {
+	if dy1 > 0 {
 		for i := y1; i <= y2; i++ {
-			ax := float64(x1+(i-y1)) * dax_step
-			bx := float64(x1+(i-y1)) * dbx_step
+			ax := float64(x1) + float64(i-y1)*dax_step
+			bx := float64(x1) + float64(i-y1)*dbx_step
 
 			tex_su := u1 + float64(i-y1)*du1_step
 			tex_sv := v1 + float64(i-y1)*dv1_step
@@ -301,18 +300,77 @@ func texturedTriangle(x1, y1 int, u1, v1 float64,
 				tex_sv, tex_ev = tex_ev, tex_sv
 			}
 
-			tex_u = tex_su
-			tex_v = tex_sv
+			tex_u := tex_su
+			tex_v := tex_sv
 
 			tstep := 1.0 / (bx - ax)
 			t := 0.0
 
-			for j := ax; j <= bx; j++ {
+			for j := ax; j < bx; j++ {
 				tex_u = (1.0-t)*tex_su + t*tex_eu
 				tex_v = (1.0-t)*tex_sv + t*tex_ev
 
-				if tex_w > pDepthBuffer[i*ScreenWidth()+j] {
+				// Draw(j, i, tex->SampleGlyph(tex_u / tex_w, tex_v / tex_w), tex->SampleColour(tex_u / tex_w, tex_v / tex_w));
+				screen.Set(int(j), i, tex.RGBA64At(int(tex_u), int(tex_v)))
+				// log.Println(j, i)
+				t += tstep
+			}
+		}
 
+		dy1 = y3 - y2
+		dx1 = x3 - x2
+		dv1 = v3 - v2
+		du1 = u3 - u2
+
+		if dy1 > 0 {
+			dax_step = float64(dx1) / math.Abs(float64(dy1))
+		}
+
+		if dy2 > 0 {
+			dbx_step = float64(dx2) / math.Abs(float64(dy2))
+		}
+
+		du1_step = 0
+		dv1_step = 0
+
+		if dy1 > 0 {
+			du1_step = du1 / math.Abs(float64(dy1))
+		}
+
+		if dy1 > 0 {
+			dv1_step = dv1 / math.Abs(float64(dy1))
+		}
+
+		if dy1 > 0 {
+			for i := y2; i <= y3; i++ {
+				ax := float64(x2) + float64(i-y2)*dax_step
+				bx := float64(x1) + float64(i-y1)*dbx_step
+
+				tex_su := u2 + float64(i-y2)*du1_step
+				tex_sv := v2 + float64(i-y2)*dv1_step
+
+				tex_eu := u1 + float64(i-y1)*du2_step
+				tex_ev := v1 + float64(i-y1)*dv2_step
+
+				if ax > bx {
+					ax, bx = bx, ax
+					tex_su, tex_eu = tex_eu, tex_su
+					tex_sv, tex_ev = tex_ev, tex_sv
+				}
+
+				tex_u := tex_su
+				tex_v := tex_sv
+
+				tstep := 1.0 / (bx - ax)
+				t := 0.0
+
+				for j := ax; j < bx; j++ {
+					tex_u = (1.0-t)*tex_su + t*tex_eu
+					tex_v = (1.0-t)*tex_sv + t*tex_ev
+
+					// Draw(j, i, tex->SampleGlyph(tex_u / tex_w, tex_v / tex_w), tex->SampleColour(tex_u / tex_w, tex_v / tex_w));
+					screen.Set(int(j), i, tex.RGBA64At(int(tex_u), int(tex_v)))
+					t += tstep
 				}
 			}
 		}
@@ -483,8 +541,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 			}
 			newTriangles = len(listTriangles)
 		}
-		for _, triProjected := range listTriangles {
-			drawTriangle(screen, &triProjected)
+		for _, t := range listTriangles {
+			texturedTriangle(int(t.p[0].x), int(t.p[0].y), t.t[0].u, t.t[0].v,
+				int(t.p[1].x), int(t.p[1].y), t.t[1].u, t.t[1].v,
+				int(t.p[2].x), int(t.p[2].y), t.t[2].u, t.t[2].v, g.tex, screen)
+
+			// drawTriangle(screen, &t)
 			trianglesDrawn++
 		}
 	}
